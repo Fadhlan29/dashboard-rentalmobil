56d461a34f9bff22b590274a8006b38d0f2c8b3a
Risqi Andryana S <109847964+Risqiyyyy@users.noreply.github.com>
2023-08-29 16:04:22 +0700

    update login register

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/DashboardStyles/style.css'
 import ChartDashboard from '../components/ChartDashboard'
 import { NavbarDashboard, Sidebar } from '../components'
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { useNavigate  } from "react-router-dom";
+
 
 const Dashboard = () => {
+  const [username, setName] = useState('');
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    refreshToken();
+  },[]);
+
+  const refreshToken = async() => {
+    try {
+      const response = await axios.get('http://localhost:5000/token');
+      console.log(response);
+      // setToken(response.data.accessToken);
+      // const decoded = jwt_decode(response.data.accessToken);
+    } catch (error) {
+      
+    }
+  }
+
 
   const dataChart = {
     data1: [80, 5, 25, 65, 8, 79, 4, 40, 78, 1, 21, 50, 100, 55, 6],
-
-data2: [30, 120 , 35, 10, 90, 5, 40, 6, 50, 20, 78, 3, 90, 8, 65],
-
-data3: [5, 40, 10, 30, 15, 50, 3, 45, 20, 55, 6, 25, 35, 8, 60],
-
-data4: [55, 3, 40, 20, 50, 8, 65, 6, 35, 10, 45, 25, 30, 15, 60]
+    data2: [30, 120 , 35, 10, 90, 5, 40, 6, 50, 20, 78, 3, 90, 8, 65],
+    data3: [5, 40, 10, 30, 15, 50, 3, 45, 20, 55, 6, 25, 35, 8, 60],
+    data4: [55, 3, 40, 20, 50, 8, 65, 6, 35, 10, 45, 25, 30, 15, 60]
   }
 
   return (
